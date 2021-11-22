feb0cfec3f3a3706a32337130271244812790d68
Alexey <work.alexeykudimov@gmail.com>
2021-11-22 12:18:18 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from '../auth/auth.module';
+import { ProfileModule } from '../profile/profile.module';
+import { NotfoundComponent } from '../shared/components/notfound/notfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    ProfileModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/auth/auth-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component'
+import { RegistrationComponent } from './components/registration/registration.component'
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component'
+
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'registration', component: RegistrationComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
